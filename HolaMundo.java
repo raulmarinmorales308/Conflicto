f65e98bf35b3fc09fdbcf2b5a7a05ce8a90c4a90
raulmarinmorales308 <rmarmor308@iesalmudeyne.es>
2024-10-23 09:15:59 +0200

    Confirmando cambios en la rama de main - Version 2

--- HolaMundo.java
+++ HolaMundo.java
@@ -1,6 +1,6 @@
 public class HolaMundo {
 
     public static void main(String[] args) {
-        System.out.println("Hola Mundo");
+        System.out.println("Hola Mundo en la rama de main");
     }
 }
